8b4426b7e8ff1e9295b2c4fc27cc77ca499c793d
KDE360 <tle360@gmail.com>
2014-09-18 23:41:13 -0700

    created seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-
+  get 'seller' => 'listings#seller'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
